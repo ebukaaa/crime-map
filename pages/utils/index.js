0feac1d44d1513c9f6bb404d1571da3cd4bf072c
Ebuks <ebukaa@protonmail.com>
2021-03-24 14:02:20 +0000

    change tests and added optional url

--- pages/utils/index.js
+++ pages/utils/index.js
@@ -12,9 +12,13 @@ let putError;
 
 // const url = `https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=${new Date().getFullYear()}-01`;
 // `${process.env.NEXT_PUBLIC_HOST}/api/crimes/get`;
-const url = `${
-  process.env.NEXT_PUBLIC_CRIME_API
-}?lat=52.629729&lng=-1.131592&date=${new Date().getFullYear()}-01`;
+const latitude = 51.509865;
+const longitude = -0.118092;
+const url = process.env.NEXT_PUBLIC_CRIME_API
+  ? `${
+      process.env.NEXT_PUBLIC_CRIME_API
+    }?lat=${latitude}&lng=${longitude}&date=${new Date().getFullYear()}-01`
+  : "https://data.police.uk/api/crimes-street/all-crime";
 
 export async function fetcher(...args) {
   return fetch(...args).then((response) => response?.json());
